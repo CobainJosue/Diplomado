afcb06406e6a7d6593f7a863b844f04be455a574
CobainJosue <chivasagrada@live.com.mx>
2018-08-31 12:17:26 -0500

    Entregable iSpy (Scroll view) de libro App Development with Swift

---
+++ Entregables/iSpy/iSpy/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  iSpy
+//
+//  Created by Josue Quiñones on 31/08/18.
+//  Copyright © 2018 Josue Quiñones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+        updateZoomFor(size: view.bounds.size)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
