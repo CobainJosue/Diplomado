dbfefae8a4c95068598ed807fba9b6281c68ab7c
CobainJosue <chivasagrada@live.com.mx>
2018-09-07 15:50:08 -0500

    Terminado parte 2 del To Do List

--- ToDoList/ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoList/ToDoTableViewController.swift
@@ -12,8 +12,28 @@ class ToDoTableViewController: UITableViewController {
     
     var todos = [ToDo]()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let savedToDos = ToDo.loadToDos() {
+            todos = savedToDos
+        } else {
+            todos = ToDo.loadSampleToDos()
+        }
+        
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return todos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier") else {
+            fatalError("Could not dequeue a cell")
+        }
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        return cell
     }
     
 }
