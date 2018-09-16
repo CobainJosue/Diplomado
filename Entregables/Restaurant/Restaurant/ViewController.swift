bbcd1e7006e095330cd80aa23771877589bcbcae
CobainJosue <chivasagrada@live.com.mx>
2018-09-15 21:39:37 -0500

    Restaurant proyect configured

---
+++ Entregables/Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Josue Quiñones on 9/15/18.
+//  Copyright © 2018 Josue Quiñones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
